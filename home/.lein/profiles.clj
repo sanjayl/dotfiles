db5b7029cb02541d86ec34c625d420a99adccb54
sanjayl <sanjay.linganna@gmail.com>
2015-06-19 01:04:11 -0400

    pared down vinyasa profile

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,18 +1,18 @@
 {:user 
- {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
+ {:plugins      [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                  [im.chit/vinyasa "0.3.4"]
                  [leiningen #=(leiningen.core.main/leiningen-version)]]
 
-  :injections [(require '[vinyasa.inject :as inject])
-               (inject/in [vinyasa.inject :refer [inject [in inject-in]]] ;; . ns
-                          [vinyasa.lein :exclude [*project*]]
-                          [vinyasa.pull :all]
-                          [cemerick.pomegranate add-classpath get-classpath resources]
-
-                          clojure.core
-                          [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
-                          
-                          clojure.core >
-                          [clojure.pprint pprint]
-                          [clojure.java.shell sh])]}}
+  :injections   [(require '[vinyasa.inject :as inject])
+                 (inject/in [clojure.pprint [pprint p]] ;; . ns
+                            ;inject function as ./inject and
+                            ;in macro as ./inject-in
+                            [vinyasa.inject :refer [inject [in inject-in]]] 
+                            [vinyasa.lein :exclude [*project*]]
+                            [vinyasa.pull :all]
+                            
+                            clojure.core > ;; core ns with > prefix
+                            [clojure.java.shell sh]
+                            [clojure.pprint [pprint >p]]
+                            [clojure.pprint [pprint >P]])]}}
