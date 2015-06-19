bb02961ae25f6330e168c0307425cd101f249da4
sanjayl <sanjay.linganna@gmail.com>
2015-06-18 23:46:48 -0400

    default vinyasa config

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,3 +1,18 @@
 {:user 
  {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+  :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                 [im.chit/vinyasa "0.3.4"]
+                 [leiningen #=(leiningen.core.main/leiningen-version)]]
+
+  :injections [(require '[vinyasa.inject :as inject])
+               (inject/in [vinyasa.inject :refer [inject [in inject-in]]] ;; . ns
+                          [vinyasa.lein :exclude [*project*]]
+                          [vinyasa.pull :all]
+                          [cemerick.pomegranate add-classpath get-classpath resources]
+
+                          clojure.core
+                          [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
+                          
+                          clojure.core >
+                          [clojure.pprint pprint]
+                          [clojure.java.shell sh])]}}
