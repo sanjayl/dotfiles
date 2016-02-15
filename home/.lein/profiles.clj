1d6fe43673a48cd793991379ca744f94962c3e4a
sanjayl <sanjay.linganna@gmail.com>
2016-02-15 11:27:45 -0500

    Change profile from user to repl
    Saves time on startup so that you don't load all this crap in for
    typical non-interactive lein use

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user 
+{:repl
  {:plugins      [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                  [im.chit/vinyasa "0.3.4"]
@@ -6,8 +6,8 @@
 
   :injections   [(require '[vinyasa.inject :as inject])
                  (inject/in [clojure.pprint [pprint p]] ;; . ns
-                            ;inject function as ./inject and
-                            ;in macro as ./inject-in
+                                        ;inject function as ./inject and
+                                        ;in macro as ./inject-in
                             [vinyasa.inject :refer [inject [in inject-in]]] 
                             [vinyasa.lein :exclude [*project*]]
                             [vinyasa.pull :all]
