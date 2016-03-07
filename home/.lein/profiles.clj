7d4783957e40e61e05c74a64cc616b5c4da8df3b
sanjayl <sanjay.linganna@gmail.com>
2016-03-06 21:23:34 -0500

    removed cider-nrepl middleware
    now directly injected

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,6 +1,6 @@
 {:repl
- {:plugins      [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+ {;:plugins      [[cider/cider-nrepl "0.12.0-SNAPSHOT"]]
+  :dependencies [;[org.clojure/tools.nrepl "0.2.12"]
                  [im.chit/vinyasa "0.3.4"]
                  [leiningen #=(leiningen.core.main/leiningen-version)]]
 
