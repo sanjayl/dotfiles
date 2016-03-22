ad66d17f1cc9d707aef97500a0f0cac08eef1294
sanjayl <sanjay.linganna@gmail.com>
2016-03-22 10:20:39 -0400

    took out vinyasa and stuff
    probably an old config, should update it at some point.
    plus it takes a lot of time to spin up a repl with it there

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,20 +1,20 @@
-{:repl
- {:dependencies [[im.chit/vinyasa "0.3.4"]
-                 [leiningen #=(leiningen.core.main/leiningen-version)]]
+{;; :repl
+ ;; {:dependencies [[im.chit/vinyasa "0.3.4"]
+ ;;                 [leiningen #=(leiningen.core.main/leiningen-version)]]
 
-  :injections   [(require '[vinyasa.inject :as inject])
-                 (inject/in [clojure.pprint [pprint p]] ;; . ns
-                                        ;inject function as ./inject and
-                                        ;in macro as ./inject-in
-                            [vinyasa.inject :refer [inject [in inject-in]]] 
-                            [vinyasa.lein :exclude [*project*]]
-                            [vinyasa.pull :all]
-                            
-                            clojure.core > ;; core ns with > prefix
-                            [clojure.java.shell sh]
-                            [clojure.pprint [pprint >p]]
-                            [clojure.pprint [pprint >P]])]}
+ ;;  :injections   [(require '[vinyasa.inject :as inject])
+ ;;                 (inject/in [clojure.pprint [pprint p]] ;; . ns
+ ;;                                        ;inject function as ./inject and
+ ;;                                        ;in macro as ./inject-in
+ ;;                            [vinyasa.inject :refer [inject [in inject-in]]] 
+ ;;                            [vinyasa.lein :exclude [*project*]]
+ ;;                            [vinyasa.pull :all]
+ 
+ ;;                            clojure.core > ;; core ns with > prefix
+ ;;                            [clojure.java.shell sh]
+ ;;                            [clojure.pprint [pprint >p]]
+ ;;                            [clojure.pprint [pprint >P]])]}
  :cider
- {:plugins      [[refactor-nrepl "2.2.0-SNAPSHOT"]
+ {:plugins      [;;[refactor-nrepl "2.2.0-SNAPSHOT"]
                  [cider/cider-nrepl "0.12.0-SNAPSHOT"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
